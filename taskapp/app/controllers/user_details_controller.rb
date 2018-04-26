6410b29bb7b632cfe61c645b1bf58ad50bd86e75
frank marks <hdx48s@yahoo.co.jp>
2018-04-26 11:45:28 +0900

    getting data of ActiveRecord changed about key

--- taskapp/app/controllers/user_details_controller.rb
+++ taskapp/app/controllers/user_details_controller.rb
@@ -1,10 +1,12 @@
 class UserDetailsController < ApplicationController
 
   def index
-    @user = Review.all
-
+    #render plain: params[:user_id].inspect
+    
+    @ra = Project.joins("LEFT OUTER JOIN reviews ON projects.id = reviews.project_id LEFT OUTER JOIN user_items ON user_items.user_id = reviews.user_id").where("reviews.user_id = ?" , params[:user_id]).select("projects.*, reviews.* , user_items.*")
+    
+    @r = Project.joins("LEFT OUTER JOIN reviews ON projects.id = reviews.project_id LEFT OUTER JOIN user_items ON user_items.user_id = reviews.user_id").where("reviews.project_id = ?" , params[:id]).select("reviews.* , user_items.*")
   end
-
   def new
   end
 end
