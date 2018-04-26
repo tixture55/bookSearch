68849e1c9e864d0bef487f9f3954510889232685
frank marks <hdx48s@yahoo.co.jp>
2018-04-26 19:44:36 +0900

    insert clicked user function added in def show

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -1,8 +1,7 @@
 
 class ProjectsController < AuthorizedController
   PER = 5
-  before_action :set_project, only: [:show,:edit, :update, :destroy] ,except: [:user_detail]
-  #before_action :set_project, except: :user_detail
+  before_action :set_project, only: [:show,:edit, :update, :destroy]
 
   def index
     
@@ -24,6 +23,11 @@ class ProjectsController < AuthorizedController
   end
   def show
     #render plain: params[:id].inspect
+    @click = Click.find_by_project_id(params[:id])
+    params[:project_id] = params[:id] 
+    params[:user_id] = 1
+    @clicked = Click.new(clicked_params)
+    @clicked.save
     @user = current_user
     @r = Project.joins("LEFT OUTER JOIN reviews ON projects.id = reviews.project_id LEFT OUTER JOIN user_items ON user_items.user_id = reviews.user_id").where("reviews.project_id = ?" , params[:id]).select("projects.* , reviews.* , user_items.*")
     @cart = Project.where("id = ? ", params[:id])
@@ -52,6 +56,7 @@ class ProjectsController < AuthorizedController
 		
   def update
     if @project.update(project_params)
+      @project.save
       redirect_to projects_path
     else
       render 'edit'
@@ -65,9 +70,14 @@ class ProjectsController < AuthorizedController
   private
       
   def project_params
+    
     params.require(:projects).permit(:title,:price,:pict_path)
   end
  
+  def clicked_params
+    params.permit(:project_id,:user_id)
+  end
+  
   def set_project
     @project = Project.find(params[:id])
   end
